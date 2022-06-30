c015bb5c8de7a9bbea2080f66cad7bf08d72be3a
adiehl96 <floaty.press-0k@icloud.com>
2022-06-30 12:57:39 +0200

    refactored code

--- pyrfm/utils/CalcBinErrorStats.py
+++ pyrfm/utils/CalcBinErrorStats.py
@@ -1,20 +1,38 @@
-from utils.CalcAUC import calc_auc
-from utils.CalcKLScore import calc_kl_score
-from utils.CalcClassScore import calc_class_score
-from utils.CalcClassifierScore import calc_classifier_score
-from utils.CalcRMSE import calc_rmse
+from sklearn import metrics
+from sklearn.metrics import mean_squared_error, log_loss
+import numpy as np
+
+
+def calc_class_score(probs, links):
+    if probs.size != 0:
+        class_score = np.sum(links * probs + (1 - links) * (1 - probs))
+        class_score = class_score / len(links)
+        return class_score
+    else:
+        return -1
+
+
+def calc_classifier_score(probs, links):
+    if probs.size != 0:
+        classifier_score = np.sum(links * (probs < 0.5) + (1 - links) * (probs >= 0.5))
+        classifier_score = classifier_score / len(links)
+        return classifier_score
+    else:
+        return -1
 
 
 def calc_bin_error_stats(probs, links):
-    auc, _, _ = calc_auc(probs, links)
-    kls = calc_kl_score(probs, links)
+    fpr, tpr, _ = metrics.roc_curve(links, probs)
+    auc = metrics.auc(fpr, tpr)
+    kls = -log_loss(links, probs, normalize=False, labels=[0, 1]) / len(links)
     cls = calc_class_score(probs, links)
     classifier_error = calc_classifier_score(probs, links)
-    rmse = calc_rmse(probs, links)
-    return {
+    rmse = mean_squared_error(links, probs, squared=False)
+    results = {
         "auc": auc,
         "kls": kls,
         "cls": cls,
         "classifier_error": classifier_error,
         "rmse": rmse,
     }
+    return results
--- pyrfm/utils/CalcRMSE.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-
-def calc_rmse(predictions, truth):
-    if truth.size != 0:
-        rmse = np.sqrt(np.mean(np.square(predictions - truth)))
-        return rmse
-    else:
-        return -1
